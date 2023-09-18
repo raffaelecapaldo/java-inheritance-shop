94aa243cc39059d2441bd802d2313bd4041d08e5
raffaelecapaldo <raffaelecpl@gmail.com>
2023-09-18 14:46:20 +0200

    Added getter and setter

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -2,4 +2,56 @@ package org.lessons.java.shop;
 
 public class Prodotto {
 
+	private String codice;
+	private String nome;
+	private String marca;
+	private double prezzo;
+	private double iva;
+	
+	
+	public String getCodice() {
+		return codice;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getMarca() {
+		return marca;
+	}
+	
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	
+	public double getPrezzo() {
+		return prezzo;
+	}
+	
+	public String getPrezzoPubblico() {
+		return String.format("%.2f", (prezzo+iva));
+	}
+	
+	public void setPrezzo(double prezzo) {
+		this.prezzo = prezzo;
+	}
+	
+	public double getIva() {
+		return iva;
+	}
+	
+	public void setIva(double iva) {
+		this.iva = iva;
+	}
+	
+	
+	
+	
+	
+	
 }
