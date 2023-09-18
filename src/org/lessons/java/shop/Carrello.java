e5f484ea99ccccad0c2087d43c994b03c28838a6
raffaelecapaldo <raffaelecpl@gmail.com>
2023-09-18 15:22:30 +0200

    Added Carrello

---
+++ src/org/lessons/java/shop/Carrello.java
@@ -0,0 +1,18 @@
+package org.lessons.java.shop;
+
+import java.util.Scanner;
+
+public class Carrello {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("1: SMARTPHONE"
+				+ "\n2: TELEVISORE "
+				+ "\n3: CUFFIE");
+		System.out.print("Inserisci il numero corrispondete al prodotto da acquistare: ");
+	}
+	
+	
+
+}
--- src/org/lessons/java/shop/Main.java
+++
@@ -1,9 +0,0 @@
-package org.lessons.java.shop;
-
-public class Main {
-
-	public static void main(String[] args) {
-
-	}
-
-}
